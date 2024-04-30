64c711188e6f4dd29a6900dbb21a1a5dc64c9460
dngtuankiet <dngtuankiet97@gmail.com>
2024-04-30 21:46:21 +0900

    add xpr module

---
+++ hardware/riscvconsole/src/main/scala/devices/entropy/ring_generator_base.scala
@@ -0,0 +1,82 @@
+package riscvconsole.devices.entropy
+
+import chipsalliance.rocketchip.config.{Field, Parameters}
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.diplomaticobjectmodel.model._
+import freechips.rocketchip.regmapper._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util.ElaborationArtefacts
+
+case class baseLocHint(
+    x1: Int = 3,
+    y1: Int = 118,
+    x2: Int = 7,
+    y2: Int = 124
+)
+
+class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int], val entropy: Seq[Int], val locHint: baseLocHint = baseLocHint()) extends Module{
+    val io = IO(new Bundle{
+        //control
+        val iRst = Input(Bool())
+        val iEn = Input(Bool())
+        val iInit = Input(Bool())
+        //random
+        val iEntropy = Input(Vec(entropy.size, Bool()))
+        //data
+        val iBit = Input(Bool())
+        val oState = Output(UInt(size.W))
+        val oSerial = Output(Bool())
+    })
+
+    withReset(io.iRst){
+        val state = RegInit(VecInit(Seq.fill(size)(0.U(1.W))))
+
+        when(io.iEn) {
+            // Top path
+            val entropies = entropy.zip(io.iEntropy).toMap
+            (0 until size/2).reverse.foreach { i =>
+                entropies.get(i+1) match {
+                    case Some(etp) => state(i) := state(i+1) ^ etp
+                    case None => state(i) := state(i+1)
+                }
+            }
+
+            // Bottom path
+            val taps = poly.zip(src)
+            (size/2 until size).reverse.foreach { i =>
+                taps.find(tap => tap._1 + tap._2 == i) match {
+                    case Some(tap) => state(i-1) := state(tap._2) ^ state(i)
+                    case None => state(i-1) := state(i)
+                }
+            }
+
+            // Inject state from last register
+            state(size-1) := Mux(io.iInit, io.iBit, state(0))
+        }
+
+        io.oState := state.asUInt
+        io.oSerial := state(0)
+    }
+    
+
+    ElaborationArtefacts.add(
+        "ring_generator_base" + ".vivado.xdc",
+        {
+            val xdcPath = pathName.split("\\.").drop(1).mkString("/")+"/"
+            println(s"Test ring gennerator base in ${pathName} <> ${xdcPath}")
+
+            val pblock =
+                s"""create_pblock ring_generator_base
+                |add_cells_to_pblock [get_pblocks ring_generator_base] [get_cells ${xdcPath}*]
+                |add_cells_to_pblock [get_pblocks ring_generator_base] [get_cells ${xdcPath}]
+                |resize_pblock [get_pblocks ring_generator_base] -add {SLICE_X${locHint.x1}Y${locHint.y1}:SLICE_X${locHint.x2}Y${locHint.y2}}
+                |set_property IS_SOFT FALSE [get_pblocks ring_generator_base]
+                |""".stripMargin
+
+            pblock
+        }
+    )//ElaborationArtefacts
+}
---
+++ hardware/riscvconsole/src/main/scala/devices/entropy/xpr.scala
@@ -0,0 +1,221 @@
+package riscvconsole.devices.entropy
+
+import chipsalliance.rocketchip.config.{Field, Parameters}
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.diplomaticobjectmodel.model._
+import freechips.rocketchip.regmapper._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util.ElaborationArtefacts
+import riscvconsole.devices.entropy._
+
+class XPR(val size: Int = 16) extends Module{
+    val io = IO(new Bundle{
+        //for XPRSlice
+        val iR = Input(Bool())
+        val i1 = Input(Bool())
+        val i2 = Input(Bool())
+        //for base
+        val iRst = Input(Bool())
+        val iEn = Input(Bool())
+        // val iInit = Input(Bool()) //use seed or not
+        // val iSeed = Input(UInt(size.W))
+        val iDelay = Input(UInt(32.W)) //for stablizing
+        //read out
+        val iNext = Input(Bool())
+        val oValid = Output(Bool())
+        // val oReady = Output(Bool())
+        val oValue = Output(UInt(32.W))
+    })
+
+    //Ring Generator Base
+    val poly = Seq(10,7,4)
+    val src = Seq(3,4,6)
+    val entropy = Seq(7,5,2,1)
+    val baseLocHint = new baseLocHint
+    val xpr_base = Module(new RingGeneratorBase(size, poly, src, entropy, baseLocHint))
+
+    //xpr_base control
+    xpr_base.io.iRst := io.iRst
+    xpr_base.io.iEn := io.iEn //TODO: use FSM to inject seed later
+    xpr_base.io.iInit := false.B //TODO: use FSM to inject seed later
+    xpr_base.io.iBit := false.B //TODO: use FSM to inject seed later
+
+    //XPR
+    val x = 0
+    val y = 121
+    val sliceLocHints = Seq.tabulate(2)(i => new sliceLocHint(x+i, y))
+    val xpr_slice = Seq.tabulate(2)(i =>
+        Module(new XPRSlice(true, sliceLocHints(i), s"xpr_slice_$i")).suggestName(s"xpr_slice_$i")
+    )
+
+
+    (0 until xpr_slice.size).foreach { i =>
+        xpr_slice(i).io.iR := io.iR
+        xpr_slice(i).io.i1 := io.i1
+        xpr_slice(i).io.i2 := io.i2
+    }
+
+    // xpr_base.io.iEntropy(0) := xpr_slice.io.out1
+    // xpr_base.io.iEntropy(1) := xpr_slice.io.out2
+    // xpr_base.io.iEntropy(2) := xpr_slice.io.out1
+    // xpr_base.io.iEntropy(3) := xpr_slice.io.out2
+    xpr_base.io.iEntropy.zip(xpr_slice.map(_.io.out1) ++ xpr_slice.map(_.io.out2)).foreach { case (input, output) =>
+      input := output
+    }
+
+    //delay counter - initial wait time for calibration
+    val tick = RegInit(false.B)
+    val delayCnt = RegInit(0.U(32.W))
+
+    when(io.iRst){
+        delayCnt := 0.U
+        tick := false.B
+    }.elsewhen(io.iEn && !tick) {
+        delayCnt := Mux(delayCnt === io.iDelay, 0.U, delayCnt + 1.U)
+        tick := delayCnt === io.iDelay
+    }
+
+    //shift reg to collect outputs
+    val shiftReg = RegInit(0.U(32.W))
+    val collectCnt = RegInit(0.U(5.W))
+    val valid = RegInit(false.B)
+    val ready = RegInit(true.B)
+
+    def risingedge(x: Bool) = x && !RegNext(x)
+    val nextTrigger = risingedge(io.iNext)
+
+    when(io.iRst || nextTrigger){ //restart sampling when reset or ready to sample
+        collectCnt := 0.U //reset counter when sampling is disable and restart another sampling
+        valid := false.B // reset valid when not sampling
+    }.otherwise{
+        when(tick && !valid) {
+            shiftReg := Cat(xpr_base.io.oSerial, shiftReg(31, 1))
+            collectCnt := collectCnt + 1.U
+            valid := collectCnt === 31.U //valid read data when counter reaches 31
+        }
+    }
+
+    io.oValue := Mux(valid, shiftReg, 0.U)
+    io.oValid := valid
+
+    //TODO:
+    //Control FSM to initialize challenge and read outputs
+}
+
+case class XPRParams(address: BigInt)
+
+// declare register-map structure
+object XPRCtrlRegs {
+  val control     = 0x00
+  val status      = 0x04
+  val delay       = 0x08
+  val random      = 0x0C
+}
+
+// mapping between HW ports and register-map
+abstract class XPRmod(busWidthBytes: Int, c: XPRParams)(implicit p: Parameters)
+  extends RegisterRouter(
+    RegisterRouterParams(
+      name = "xpr",
+      compat = Seq("console,xpr0"),
+      base = c.address,
+      beatBytes = busWidthBytes))
+{
+  lazy val module = new LazyModuleImp(this) {
+    // HW instantiation
+    val mod = Module(new XPR(16))
+
+    // declare inputs
+    val ir     = RegInit(false.B)
+    val i1     = RegInit(false.B)
+    val i2     = RegInit(false.B)
+    val rst    = RegInit(false.B)
+    val enable = RegInit(false.B)
+    val next   = RegInit(false.B)
+    val delay  = RegInit(0.U(32.W))
+    // mapping inputs
+    mod.io.iR := ir
+    mod.io.i1 := i1
+    mod.io.i2 := i2
+    mod.io.iRst   := reset.asBool || rst
+    mod.io.iEn    := enable
+    mod.io.iNext  := next
+    mod.io.iDelay := delay
+
+    // declare outputs
+    val valid  = Wire(Bool())
+    val value = Wire(UInt(32.W))
+    // mapping outputs
+    valid  := mod.io.oValid
+    // rand := RegEnable(mod.io.oRand, valid)
+    value := mod.io.oValue
+
+    // map inputs & outputs to register positions
+    val mapping = Seq(
+      XPRCtrlRegs.control -> Seq(
+        RegField(1, enable, RegFieldDesc("trigger", "XPR enable")),
+        RegField(1, next, RegFieldDesc("trigger", "XPR next")),
+        RegField(1),
+        RegField(1, ir, RegFieldDesc("trigger", "XPR iR")),
+        RegField(1, i1, RegFieldDesc("trigger", "XPR i1")),
+        RegField(1, i2, RegFieldDesc("trigger", "XPR i2")),
+        RegField(2),
+        RegField(1, rst, RegFieldDesc("rst", "XPR reset", reset = Some(0)))
+      ),
+      XPRCtrlRegs.status -> Seq(
+        RegField.r(1, valid, RegFieldDesc("valid", "XPR data valid", volatile = true))
+      ),
+      XPRCtrlRegs.delay -> Seq(RegField(32, delay, RegFieldDesc("delay", "delay time for calibrartion XPR"))),
+      XPRCtrlRegs.random -> Seq(RegField.r(32, value, RegFieldDesc("output", "output for XPR", volatile = true))),
+    )
+    regmap(mapping :_*)
+    val omRegMap = OMRegister.convert(mapping:_*)
+  }
+}
+
+// declare TileLink-wrapper class for XPR-module
+class TLXPR(busWidthBytes: Int, params: XPRParams)(implicit p: Parameters)
+  extends XPRmod(busWidthBytes, params) with HasTLControlRegMap
+
+// this will auto +1 ID if there are many XPR modules
+object XPRID {
+  val nextId = {
+    var i = -1; () => {
+      i += 1; i
+    }
+  }
+}
+
+// attach TLXPR to a bus
+case class XPRAttachParams
+(
+  device: XPRParams,
+  controlWhere: TLBusWrapperLocation = PBUS)
+{
+  def attachTo(where: Attachable)(implicit p: Parameters): TLXPR = where {
+    val name = s"xpr_${XPRID.nextId()}"
+    val cbus = where.locateTLBusWrapper(controlWhere)
+    val xpr = LazyModule(new TLXPR(cbus.beatBytes, device))
+    xpr.suggestName(name)
+
+    cbus.coupleTo(s"device_named_$name") { bus =>
+      (xpr.controlXing(NoCrossing)
+        := TLFragmenter(cbus)
+        := bus )
+    }
+    xpr
+  }
+}
+
+// declare trait to be called in a system
+case object PeripheryXPRKey extends Field[Seq[XPRParams]](Nil)
+
+// trait to be called in a system
+trait HasPeripheryXPR { this: BaseSubsystem =>
+  val xprNodes = p(PeripheryXPRKey).map { ps =>
+    XPRAttachParams(ps).attachTo(this)
+  }
+}
