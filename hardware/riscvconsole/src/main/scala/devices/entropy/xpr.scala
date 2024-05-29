b09da2b6121f6a2605d0a29449865f6e58cd4bae
dngtuankiet <dngtuankiet97@gmail.com>
2024-05-29 10:54:54 +0900

    placement test7

--- hardware/riscvconsole/src/main/scala/devices/entropy/xpr.scala
+++ hardware/riscvconsole/src/main/scala/devices/entropy/xpr.scala
@@ -42,11 +42,20 @@ class XPR(val size: Int = 16) extends Module{
     // val entropy = Seq(7) //Test#4 > Test#3 > Test#2 & Test#1
     // val entropy = Seq(7,5) //Test#5 - with only 1 EC
     // val entropy = Seq(7,5,2) //Test#6 - with only 1 EC
-    // val entropy = Seq(15,7,5,2) //Test#7 - fixed placement of base, including all xor gate, use two 2EC
+    val entropy = Seq(15,7,5,2) //Test#7 - fixed placement of base, including all xor gate, use two 2EC
     // val entropy = Seq(7) //Test#8 - fixed placement of base, including all xor gate
     // val entropy = Seq(7,5,2) //Test#9 - fixed placement of base, including all xor gate, 1 EC for all etp
-    val entropy = Seq(15,12,7,5) //Test#10 - fixed placement of base, including all xor gate, 2 EC
-    val baseLocHint = new baseLocHint
+    // val entropy = Seq(15,12,7,5) //Test#10 - fixed placement of base, including all xor gate, 2 EC
+
+    //Placement settings
+    // val baseLocHint = new baseLocHint(loc_x=30, loc_y=149) //Test7-mid1
+    // val baseLocHint = new baseLocHint(loc_x=2, loc_y=198) //Test7-topleft
+    // val baseLocHint = new baseLocHint(loc_x=74, loc_y=198) //Test7-topright
+    // val baseLocHint = new baseLocHint(loc_x=30, loc_y=197) //Test7-top
+    // val baseLocHint = new baseLocHint(loc_x=2, loc_y=13) //Test7-botleft
+    // val baseLocHint = new baseLocHint(loc_x=74, loc_y=3) //Test7-botright
+    val baseLocHint = new baseLocHint(loc_x=30, loc_y=2) //Test7-bot
+
     val xpr_base = Module(new RingGeneratorBase(size, poly, src, entropy, baseLocHint))
 
     //xpr_base control
@@ -60,8 +69,22 @@ class XPR(val size: Int = 16) extends Module{
     //XPR
     // val x = 0
     // val y = 121
-    val x = 28
-    val y = 149
+
+    // val x = 28 //Test7-mid1
+    // val y = 149 //Test7-mid1
+    // val x = 0 //Test7-topleft
+    // val y = 198 //Test7-topleft
+    // val x = 72 //Test7-topright
+    // val y = 198 //Test7-topright
+    // val x = 28 //Test7-top
+    // val y = 197 //Test7-top
+    // val x = 0 //Test7-botleft
+    // val y = 13 //Test7-botleft
+    // val x = 72 //Test7-botright
+    // val y = 3 //Test7-botright
+    val x = 28 //Test7-bot
+    val y = 2 //Test7-bot
+
     val slice_num = 2
     val sliceLocHints = Seq.tabulate(slice_num)(i => new sliceLocHint(x+i, y))
     val xpr_slice = Seq.tabulate(slice_num)(i =>
@@ -75,10 +98,10 @@ class XPR(val size: Int = 16) extends Module{
     }
     
     // 1 EC source - 3 entropies - one for all
-    val entropy_src = Cat(xpr_slice(0).io.out1, xpr_slice(0).io.out1, xpr_slice(0).io.out1)
-    xpr_base.io.iEntropy.zip(entropy_src.asBools).foreach { case (input, output) =>
-      input := output
-    }
+    // val entropy_src = Cat(xpr_slice(0).io.out1, xpr_slice(0).io.out1, xpr_slice(0).io.out1)
+    // xpr_base.io.iEntropy.zip(entropy_src.asBools).foreach { case (input, output) =>
+    //   input := output
+    // }
 
     // 1 EC source - 3 entropies - with inverter
     // val w_not0 = ~xpr_slice(0).io.out1
@@ -94,10 +117,10 @@ class XPR(val size: Int = 16) extends Module{
     //   input := output
     // }
 
-    // // auto route multiple ECs
-    // xpr_base.io.iEntropy.zip(xpr_slice.flatMap(slice => Seq(slice.io.out1, slice.io.out2))).foreach { case (input, output) =>
-    //   input := output
-    // }
+    // auto route multiple ECs
+    xpr_base.io.iEntropy.zip(xpr_slice.flatMap(slice => Seq(slice.io.out1, slice.io.out2))).foreach { case (input, output) =>
+      input := output
+    }
 
     //delay counter - initial wait time for calibration
     val tick = RegInit(false.B)
