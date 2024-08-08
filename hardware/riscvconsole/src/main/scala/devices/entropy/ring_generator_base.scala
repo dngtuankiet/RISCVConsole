451aa160553358d12224fd368f332b20380d61d9
dngtuankiet <dngtuankiet97@gmail.com>
2024-08-08 14:51:49 +0900

    reset with only iRst and enable with iEn

--- hardware/riscvconsole/src/main/scala/devices/entropy/ring_generator_base.scala
+++ hardware/riscvconsole/src/main/scala/devices/entropy/ring_generator_base.scala
@@ -36,6 +36,8 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
         val oSerial = Output(Bool())
     })
 
+    withReset(io.iRst){
+
     val xor_poly = VecInit.tabulate(poly.length){ i =>
         val xp = Module(new xilinx_xor())
         xp.suggestName(s"xp_${i}")
@@ -47,9 +49,10 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
         xe.suggestName(s"xe_${i}")
         xe.io    
     }
-
+    
     var poly_count = 0
     var etp_count = 0
+    var etp_2_poly = 0
 
     val state = RegInit(VecInit(Seq.fill(size)(0.U(1.W))))
 
@@ -71,7 +74,9 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
             case Some(tap) => {
                 xor_poly(poly_count).i0 := state(tap._2)
                 xor_poly(poly_count).i1 := state(i)
+                // xor_poly(poly_count).i2 := io.iEntropy(etp_2_poly)
                 poly_count += 1
+                // etp_2_poly += 1
                 // state(i-1) := state(tap._2) ^ state(i)
             }
             case None =>
@@ -79,10 +84,51 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
     }
 
 
+    // //Connect output of XOR to the ring
+    // poly_count = 0
+    // etp_count = 0
+    // when(io.iRst){
+    //     when(io.iEn) {
+    //         (1 until size).reverse.foreach { i =>
+    //             entropies.get(i) match {
+    //                 case Some(etp) => {
+    //                     state(i-1) := xor_etp(etp_count).out
+    //                     etp_count += 1
+    //                 }
+    //                 case None => state(i-1) := state(i)
+    //             }
+
+    //             taps.find(tap => tap._1 + tap._2 == i) match {
+    //                 case Some(tap) => {
+    //                     state(i-1) := xor_poly(poly_count).out
+    //                     poly_count += 1
+    //                 }
+    //                 case None =>
+    //             }
+    //         }
+
+    //         // Inject state from last register
+    //         state(size-1) := Mux(io.iInit, io.iBit, state(0))
+    //     }.otherwise{
+    //         for (i <- 0 until size) {
+    //             state(i) := state(i)
+    //         }
+    //     }
+
+    // }.otherwise{
+    //     for (i <- 0 until size) {
+    //         state(i) := 0.U
+    //     }
+    // }
+
+    // io.oState := state.asUInt
+    // io.oSerial := state(0)
+
+
     //Connect output of XOR to the ring
     poly_count = 0
     etp_count = 0
-    withReset(io.iRst){
+    
         when(io.iEn) {
             (1 until size).reverse.foreach { i =>
                 entropies.get(i) match {
@@ -108,8 +154,10 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
 
         io.oState := state.asUInt
         io.oSerial := state(0)
+        
     }
 
+    
 
 
 
@@ -158,7 +206,7 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
 
             val variations =(for (i <- 0 until (size/8)) yield {
             (for(j <- 0 until 8) yield{
-              s"""set_property LOC SLICE_X${locHint.loc_x}Y${locHint.loc_y-i} [get_cells ${xdcPath}state_${i*8+j}_reg]
+              s"""set_property LOC SLICE_X${locHint.loc_x+1}Y${locHint.loc_y-i} [get_cells ${xdcPath}state_${i*8+j}_reg]
                  |set_property DONT_TOUCH true [get_cells ${xdcPath}state_${i*8+j}_reg]
                  |set_property BEL ${FDRE_name(j)} [get_cells ${xdcPath}state_${i*8+j}_reg]
                  |""".stripMargin
@@ -170,7 +218,7 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
             //   val index = i
               val LUT_index = i % 4
               val LUT_inst = s"${xdcPath}xor_poly_xp_${i}/inst/LUT6_inst"
-              s"""set_property LOC SLICE_X${locHint.loc_x+1}Y${locHint.loc_y-i/4} [get_cells $LUT_inst]
+              s"""set_property LOC SLICE_X${locHint.loc_x}Y${locHint.loc_y-i/4} [get_cells $LUT_inst]
                  |set_property DONT_TOUCH true [get_cells $LUT_inst]
                  |set_property BEL ${LUT_name(LUT_index)} [get_cells $LUT_inst]
                  |""".stripMargin
@@ -180,7 +228,7 @@ class RingGeneratorBase(val size: Int = 16, val poly: Seq[Int], val src: Seq[Int
             //   val index = i
               val LUT_index = i % 4
               val LUT_inst = s"${xdcPath}xor_etp_xe_${i}/inst/LUT6_inst"
-              s"""set_property LOC SLICE_X${locHint.loc_x+1}Y${locHint.loc_y-i/4-poly_offset} [get_cells $LUT_inst]
+              s"""set_property LOC SLICE_X${locHint.loc_x}Y${locHint.loc_y-i/4-poly_offset} [get_cells $LUT_inst]
                  |set_property DONT_TOUCH true [get_cells $LUT_inst]
                  |set_property BEL ${LUT_name(LUT_index)} [get_cells $LUT_inst]
                  |""".stripMargin
